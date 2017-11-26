ef3ca6b6670bee5f5d267cdccaaf0b49b36289af
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-26 21:38:45 +0200

    Add common components

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import firebase from 'firebase';
 
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyBhaIyYf7lm32k0C5GH3sNgJ64pPQ0o4fQ",
+      authDomain: "manager-app-ba47e.firebaseapp.com",
+      databaseURL: "https://manager-app-ba47e.firebaseio.com",
+      projectId: "manager-app-ba47e",
+      storageBucket: "",
+      messagingSenderId: "88950789435"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
