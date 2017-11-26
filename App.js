98d504fda677a4e19eb5ab9dae56c1baca1fc1c4
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-26 21:54:54 +0200

    Add initial login form component

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './src/reducers';
 import firebase from 'firebase';
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
 
 export default class App extends React.Component {
@@ -11,12 +12,12 @@ export default class App extends React.Component {
   componentWillMount() {
     // Initialize Firebase
     const config = {
-      apiKey: "AIzaSyBhaIyYf7lm32k0C5GH3sNgJ64pPQ0o4fQ",
-      authDomain: "manager-app-ba47e.firebaseapp.com",
-      databaseURL: "https://manager-app-ba47e.firebaseio.com",
-      projectId: "manager-app-ba47e",
-      storageBucket: "",
-      messagingSenderId: "88950789435"
+      apiKey: 'AIzaSyBhaIyYf7lm32k0C5GH3sNgJ64pPQ0o4fQ',
+      authDomain: 'manager-app-ba47e.firebaseapp.com',
+      databaseURL: 'https://manager-app-ba47e.firebaseio.com',
+      projectId: 'manager-app-ba47e',
+      storageBucket: '',
+      messagingSenderId: '88950789435'
     };
     firebase.initializeApp(config);
   }
@@ -24,19 +25,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>Hello</Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/LoginForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Card, CardItem, Input, Button, } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardItem>
+          <Input
+            label='Email'
+            placeholder='user@email.com'
+          />
+        </CardItem>
+
+        <CardItem>
+          <Input
+            secureTextEntry
+            label='Password'
+            placeholder='password'
+          />
+        </CardItem>
+
+        <CardItem>
+          <Button>
+            Login
+          </Button>
+        </CardItem>
+      </Card>
+    );
+  }
+}
+
+
+export default LoginForm;
