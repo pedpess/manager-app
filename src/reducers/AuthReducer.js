4a713de2ef762901b04016e5ad281a5c3286a1c5
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-28 22:23:57 +0200

    Fix error showing

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -29,6 +29,7 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         user: action.payload,
+        error: '',
       };
     case LOGIN_USER_FAIL:
       return {
